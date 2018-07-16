ca23094c05ffb459946db4a709efc3d77502235e
AnaMartinez <martinezdelgadoana@gmail.com>
2018-07-16 14:00:44 +0200

    ej

--- src/com/ipartek/formacion/ejemplo1/Poligono.java
+++ src/com/ipartek/formacion/ejemplo1/Poligono.java
@@ -1,31 +1,28 @@
 package com.ipartek.formacion.ejemplo1;
 
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
+
+// ni, ny, nn, n, gn
 
 public class Poligono {
-
 	private Punto origen;
-	//private LinkedHashSet<Punto> puntos=new LinkedHashSet<>();
-	private LinkedHashMap<Integer,Punto> puntos=new LinkedHashMap<>();
-
+	private LinkedHashMap<Integer, Punto> puntos = new LinkedHashMap<>();
+	
 	public Poligono(Punto origen) {
-		this.origen =origen;
+		this.origen = origen;
 	}
 	
 	public void add(Punto punto) {
-		if(punto==null) {
+		if(punto == null)
 			throw new RuntimeException("No se admiten puntos nulos");
-		}
-		puntos.put(puntos.size(),punto);
+		
+		puntos.put(puntos.size(), punto);
 	}
 	
 	public Punto get(int indice) {
 		return puntos.get(indice);
-		
 	}
-	
-	
+
 	public Punto getOrigen() {
 		return origen;
 	}
@@ -33,10 +30,9 @@ public class Poligono {
 	public void setOrigen(Punto origen) {
 		this.origen = origen;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Poligono [origen=" + origen + ", puntos=" + puntos + "]";
 	}
-
 }
