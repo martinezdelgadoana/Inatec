eac7b56ece9153f2257f7596f84185ddb1ff8e91
AnaMartinez <martinezdelgadoana@gmail.com>
2018-07-23 08:28:45 +0200

    .

--- src/com/ipartek/formacion/video/accessodatos/VideoYoutubeArrayDAO.java
+++ src/com/ipartek/formacion/video/accessodatos/VideoYoutubeArrayDAO.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import com.ipartek.formacion.prestamolibros.accesodatos.CrudAble;
 import com.ipartek.formacion.video.pojo.VideoYoutube;
 
 public class VideoYoutubeArrayDAO implements CrudAble<VideoYoutube> {
--- src/com/ipartek/formacion/video/accessodatos/VideoYoutubeArrayDAOTest.java
+++ src/com/ipartek/formacion/video/accessodatos/VideoYoutubeArrayDAOTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.ipartek.formacion.prestamolibros.accesodatos.CrudAble;
 import com.ipartek.formacion.video.pojo.VideoYoutube;
 
 class VideoYoutubeArrayDAOTest {
