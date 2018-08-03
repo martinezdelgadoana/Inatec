ced83a0ac998304f2d21aaf597e04e2e675f5406
piscisroja <piscisroja@Akasha>
2018-08-03 20:53:04 +0200

    ahorcadoCasaVacas.Pendiente espacios en web

--- Ahorcado/src/main/java/com/ipartek/ahorcado/servlets/ComprobarLetra.java
+++ Ahorcado/src/main/java/com/ipartek/ahorcado/servlets/ComprobarLetra.java
@@ -10,17 +10,35 @@ import com.ipartek.ahorcado.palabras.CrearPalabra;
 
 public class ComprobarLetra extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static StringBuilder strBu ;
+	//public static StringBuilder strBu 
+	public static StringBuilder strBu = new StringBuilder();
     
 	public ComprobarLetra() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String letra = request.getParameter("letra");
 		
-		String palabra=CrearPalabra.rellenarPalabrasArray();		
-		strBu = new StringBuilder(palabra.length());
+		String palabra=CrearPalabra.rellenarPalabrasArray();	
+		strBu.setLength(palabra.length());
+//		
+//		char[] array = new char[palabra.length()];
+//		
+//		for (int i=0;i<array.length;i++) {
+//			array[i]='-';
+//		}
+//		
+//		for (int i=0;i<array.length;i++) {
+//			System.out.print("ARRAY   "+array[i]);
+//		}
+		//HAY QUE PONERLO EN UN METODO QUE SE LLAME CADA VEZ QUE SE LE DE BOTON EN LA PANTALLA 
+		//PUESTO QUE AKI SE VUELVE A PONER ESPACIOS
+		System.out.println("plabraLeng-"+palabra.length());
+		System.out.println("strLeng-"+strBu.length());
+		
+		//strBu = new StringBuilder(palabra.length());
 		
 		//Colocar en el strBuilder '-' para mostrar cuantos espacios tiene
 		colocarEspacios(palabra);
@@ -29,20 +47,35 @@ public class ComprobarLetra extends HttpServlet {
 			System.out.println("Letra esta en palabra en la pos "+palabra.indexOf( letra ));
 			
 			//Cambio el hueco '-' de la posicion donde esta la letra por la letra
-			strBu.insert(palabra.indexOf( letra ), letra);
-			//Se vacia todo el tiempo el strBu!!!!!!!!!
+			//strBu.insert(palabra.indexOf( letra ), letra);
+			strBu.setCharAt(palabra.indexOf( letra ), letra.charAt(0));
 			
 			System.out.println("STRBUILDER... "+strBu.toString());
 			response.sendRedirect("aJugar.jsp");
 		}else {
 			System.out.println("FALLO");
+			response.sendRedirect("aJugar.jsp");
 		}
 		 
 	}
-
+	
+	public StringBuilder strBuReturn() {
+		return strBu;
+	}
+	/*NO SE USA PORQUE NO FUNCIONA*/
 	public void colocarEspacios(String palabra) {
-		for (int i=0; i<palabra.length()-1;i++) {
-			strBu.append('-');
+		System.out.println("strBu.length();"+strBu.length());
+		System.out.println("Huecos para poner-"+palabra.length());
+
+//		for (int index = 0; index < strBu.length(); index++) {
+//		    if (strBu.charAt(index) == '-') {
+//		    	strBu.setCharAt(index, '_');
+//		    }
+//		}
+		for (int index = 0; index < strBu.length(); index++) {
+		    if (strBu.charAt(index) == '-') {
+		    	strBu.setCharAt(index, '_');
+		    }
 		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- Ahorcado/src/main/webapp/aJugar.jsp
+++ Ahorcado/src/main/webapp/aJugar.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" %>
+<%@ page import= "com.ipartek.ahorcado.servlets.ComprobarLetra" %> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -14,15 +14,18 @@
     
     <!-- el action le dice a que parte del  deployedresources/webapp/webinf/webxml-->
     <form action="comprobarLetra" method="post">
-        <fieldset>
-            <legend>Letra</legend>
+        <!-- fieldset> 
+            <legend>Letra</legend>-->
             <p>
                 <label for="letra">Letra</label>
                 <input type="text" name="letra" id="letra" />
                 	<!--value='<jsp:getProperty property="letra" name="letra"/>' /-->
             </p>
-           
-        </fieldset>
+        <!-- </fieldset> -->
+        <p>
+
+          <%= ComprobarLetra.strBu.toString() %>
+        </p>
     </form>
   
 </body>
