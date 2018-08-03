b6b76f1d35c507b6fec1fcc7598eb3c6e1c7a545
piscisroja <piscisroja@Akasha>
2018-08-03 20:59:05 +0200

    organiceimports

--- Ahorcado/src/main/java/com/ipartek/ahorcado/servlets/InicioServlet.java
+++ Ahorcado/src/main/java/com/ipartek/ahorcado/servlets/InicioServlet.java
@@ -1,6 +1,7 @@
 package com.ipartek.ahorcado.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
